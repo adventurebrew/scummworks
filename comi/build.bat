01f2080d457043dc98ef637655d484b67b96201a
BLooperZ <blooperz@users.noreply.github.com>
2022-11-07 07:13:34 +0200

    COMI: Fix build script

--- comi/build.bat
+++ comi/build.bat
@@ -1,4 +1,4 @@
-python -m nutcracker.sputm.build --ref orig\COMI userdata\COMI
+call nutcracker sputm build --ref orig\COMI.LA0 userdata\COMI
 ..\utils\md5deep64 -bz COMI.LA0 > hash.txt
 ..\utils\md5deep64 -bz COMI.LA1 >> hash.txt
 ..\utils\md5deep64 -bz COMI.LA2 >> hash.txt

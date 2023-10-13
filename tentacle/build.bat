15eebe5cafded29a98e98fc5443a5b4f2cad6029
BLooperZ <blooperz@users.noreply.github.com>
2023-10-13 16:29:22 +0300

    TENTACLE: Update fonts, texts and logo

--- tentacle/build.bat
+++ tentacle/build.bat
@@ -1,4 +1,4 @@
-python -m nutcracker.sputm.build --ref orig\TENTACLE userdata\TENTACLE
+call nutcracker sputm build --ref orig\TENTACLE.000 userdata\TENTACLE
 ..\utils\scummtr -w -g tentacle -if strings.txt -p .
 ..\utils\md5deep64 -bz TENTACLE.000 > hash.txt
 ..\utils\md5deep64 -bz TENTACLE.001 >> hash.txt

88593d76fb3ef6b4151ad65a09cd27dcc6cbb2b2
BLooperZ <blooperz@users.noreply.github.com>
2024-09-13 09:46:27 +0300

    MONKEY: Add hebrew chapter titles

--- monkey/build.bat
+++ monkey/build.bat
@@ -1,4 +1,4 @@
-python -m nutcracker.sputm.build --ref orig\MONKEY userdata\MONKEY
+call nutcracker sputm build --ref orig\monkey.000 userdata\MONKEY
 ..\utils\scummtr -w -g monkeycd -if strings.txt -p .
 ..\utils\md5deep64 -bz MONKEY.000 > hash.txt
 ..\utils\md5deep64 -bz MONKEY.001 >> hash.txt

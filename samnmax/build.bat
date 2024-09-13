f19d716e0f231c76eba68491fa6547ae702efaf1
BLooperZ <blooperz@users.noreply.github.com>
2024-09-13 09:42:41 +0300

    SAMNMAX: Update build script

--- samnmax/build.bat
+++ samnmax/build.bat
@@ -1,4 +1,5 @@
-python -m nutcracker.sputm.build --ref orig\SAMNMAX userdata\SAMNMAX
+call nutcracker sputm build --ref orig\SAMNMAX.000 userdata\SAMNMAX
+@REM call nutcracker sputm strings_inject -t strings.txt SAMNMAX.000
 ..\utils\scummtr -w -g samnmax -if strings.txt -p .
 ..\utils\md5deep64 -bz SAMNMAX.000 > hash.txt
 ..\utils\md5deep64 -bz SAMNMAX.001 >> hash.txt
